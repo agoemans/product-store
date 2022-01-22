a1d48ad0ade8c439a090618abc396328570b1858
agoemans <kewlboontjies@gmail.com>
2022-01-22 11:13:23 +0100

    Added packages and initial folder structure

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        client: path.resolve(__dirname, 'src', 'index.tsx'),
+    },
+    target: "web",
+    output: {
+        path: path.resolve(__dirname, '_build'),
+        filename: 'bundle.js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
+                }
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    },
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        historyApiFallback: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: path.resolve(__dirname, 'templates', 'index.html'),
+                filename: "./index.html"
+            })
+    ]
+};
